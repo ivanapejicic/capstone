ad68cc5cf186c935bfa26d096608bc26258a45ae
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-11-19 12:51:06 -0500

    Front-end for /user page completed. Made some changes for tablet and desktop view for page to be responsive

--- src/assets/pages/User/User.js
+++ src/assets/pages/User/User.js
@@ -1,20 +1,24 @@
 import Header from '../../components/Header/Header';
 import './User.scss';
+import car from '../../images/home.jpeg'
 
 
 function User() {
     return(
         <div className='user'>
             <Header />
-            <div className='user__top'>
-                <h1 className='user__top-title'>Ready to dive in?</h1>
-                <h2 className='user__top-subtitle'>Sign up for the fun ride</h2>
-                <button className='user__top-button'>SIGN UP</button>
+            <img className='user__photo' src={car} alt='same car that was on a home page, with luggage on top, in tropical area'></img>
+            <div className='user__content'>
+            <div className='user__content-box'>
+                <h1 className='user__content-box__title'>Ready to dive in?</h1>
+                <h2 className='user__content-box__subtitle'>Sign up for the fun ride</h2>
+                <button className='user__content-box__button'>SIGN UP</button>
             </div>
-            <div className='user__bottom'>
-                <h1 className='user__bottom-title'>Already on board?</h1>
-                <h2 className='user__bottom-subtitle'>Log in to your account</h2>
-                <button className='user__bottom-title'>LOG IN</button>
+            <div className='user__content-box'>
+                <h1 className='user__content-box__title'>Already on board?</h1>
+                <h2 className='user__content-box__subtitle'>Log in to your account</h2>
+                <button className='user__content-box__button'>LOG IN</button>
+            </div>
             </div>
         </div>
     );
