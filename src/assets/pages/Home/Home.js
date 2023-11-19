44e34b20facc66ce377f148609ecd5688a75f8ff
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-11-19 11:04:47 -0500

    Home page completed with image, title, subtitle and arrow that will take user to the next step which will be login-signup page

--- src/assets/pages/Home/Home.js
+++ src/assets/pages/Home/Home.js
@@ -1,10 +1,15 @@
 import './Home.scss';
+import homephoto from '../../images/home.jpeg';
+import arrow from '../../icons/arrow_forward.svg';
 
 function Home () {
     return(
-        <>
-        
-        </>
+        <div className='home'>
+            <img  className='home__photo' src={homephoto} alt='car with luggage on top in tropical city with palms around'></img>
+            <h1 className='home__title'>Your go-to for shared rides</h1>
+            <h3 className='home__subtitle'>Save money, connect with travelers and go green</h3>
+            <img className='home__arrow' src={arrow} alt='lttle arrow forward to click for next page'></img>
+        </div>
     );
 }
 
