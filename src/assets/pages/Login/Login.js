254b3d7f087c012e5c12141ef98048ebf6d02f01
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-11-20 14:19:49 -0500

    Created a form for user login, it still needs some minor styling to be completed

--- src/assets/pages/Login/Login.js
+++ src/assets/pages/Login/Login.js
@@ -1,9 +1,24 @@
 import './Login.scss';
 
-function Login(){
-    return(
-        <p>Login....
-        </p>
+function Login() {
+    return (
+        <div className='login'>
+            <form className='form'>
+                <label className='form__label' htmlFor="username">Username:</label>
+                <input className='form__input' type="text" id="username" name="username" required />
+                <br />
+                <label className='form__label' htmlFor="password">Password:</label>
+                <input className='form__input' type="password" id="password" name="password" required />
+                <br />
+                <div className='form__links'>
+                    <label>
+                        <input type="checkbox" /> Remember me
+                    </label>
+                    <a href="/forgot-password">Forgot password?</a>
+                </div>
+                <button className='form__button' type="button">Login</button>
+            </form>
+        </div>
     );
 }
 
