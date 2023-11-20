866bd89d6c95609431512100c0796a7ac2d67782
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-11-20 15:24:02 -0500

    Added styling for signup page, to have same styling for form as login page

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
           <Route path= '/about' element={<About />} />  
           <Route path= '/login' element={<Login />} />  
           <Route path= '/signup' element={<Signup />} /> 
-
         </Routes>      
       </BrowserRouter>
     </div>
